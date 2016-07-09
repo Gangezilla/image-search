1349321152f9bc5d548be8a18644ebd8a5178af0
Scott <scottgangemi@gmail.com>
2016-07-09 13:51:28 +1000

    Fixed up problems with writing to db, added blurb about usage

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ var search=require('./search');
 var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOLAB_URI_IMAGE;
 
+var database;
+var memObject = {
+	"query": '',
+	"time": ''
+};
+
 MongoClient.connect(url, function(err, db) {
-    datab = db;
+    database = db;
     if (err) {
         console.log(err);
     } else {
@@ -22,10 +28,13 @@ app.listen(port, function() {
 });
 
 app.get("/", function(req, res) {
-	res.send('<h1> Hello. Search? </h1> <p> This is a small tool to demonstrate an "image search abstraction layer" that acts as the sort of logic behind an image search. In the address bar up top, you can type in a query after the .com and you will see a list of images. You can paginate through these by adding "?offset=20" after your query. You can check out some other things that have been searched for by typing in  ');
+	res.send('<h1> Hello. Search? </h1> <p> This is a small tool to demonstrate an "image search abstraction layer" that acts as the sort of logic behind an image search. In the address bar up top, you can type in a query after the .com and you will see a list of images. You can paginate through these by adding "?offset=20" after your query. For example ".com/test?offset=5" You can check out some other things that have been searched for by typing in "...com/api/latest". All code is up on GitHub at https://github.com/Gangezilla/image-search so feel free to check it out.');
 });
 
 app.get('/:query', function(req, res) {
+	memObject.query=req.params.query;
+ 	memObject.time=new Date();
+ 	mongoInsert(database, 'searches', memObject, function() {});
 	console.log(req.query.offset);
 	var results= [];
 	search.search(req.params.query, req.query.offset, function(body) {
@@ -39,3 +48,30 @@ app.get('/:query', function(req, res) {
 		res.send(results);
 	});
 });
+
+app.get('/api/latest', function(req,res) {
+ 	mongoFind(database, 'searches', function(err, data) {
+ 		if (err) throw err;
+ 		else {
+ 			res.send(data);
+ 		}
+ 	});
+ });
+
+ function mongoInsert(db, collection_name, data, cb) {
+     var collection = db.collection(collection_name);
+     collection.insert(data, function(err, res) {
+         if (err) {
+             console.log(err);
+         } else {
+             console.log('Inserted into the ' + collection_name + ' collection');
+             cb(res);
+         }
+     });
+ }
+
+ function mongoFind(db, collection_name, cb) {
+         console.log('Searching for link...');
+         var collection = db.collection(collection_name);
+         collection.find().limit(30).toArray(cb);
+ }
